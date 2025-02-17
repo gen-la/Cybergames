efbacc2ad6574d9e0d0808a2d8d2f4d9ef3ee334
gen-la <147514418+gen-la@users.noreply.github.com>
2025-02-17 13:21:32 +0100

    databas update

--- Migrations/20250217121401_AddGamesTable.Designer.cs
+++ Migrations/20250217121401_AddGamesTable.Designer.cs
@@ -4,6 +4,7 @@ using Cybergames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cybergames.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250217121401_AddGamesTable")]
+    partial class AddGamesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
