195c876a94d65a59d4469a7e7df73e6fc4ba97c2
ma224ip <mostafa1996@live.se>
2025-03-12 16:06:15 +0100

    d

--- obj/Debug/net8.0/Cybergames.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cybergames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cybergames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c372706f22b0250ff43f949e502db892612782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abcc3ded1916ea00d57a7f7ff907310128ef0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cybergames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cybergames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
